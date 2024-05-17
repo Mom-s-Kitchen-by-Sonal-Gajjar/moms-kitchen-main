cb3376581159a7a4385ab09417660711897684fb
Shreyansh Gajjar <shreyanshgajjar2005@gmail.com>
2024-05-17 16:55:54 +0530

    init1

---
+++ moms-app/components/navbar.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+export const Navbar = ()=>{
+    return <div className="border-b flex justify-between px-10 py-4">
+        <div className="flex items-center">
+                <div className="flex flex-col justify-center cursor-pointer">
+                    Medium
+                </div>
+        </div>
+        <div>
+            <div className="flex flex-cols-2">
+                    <div className="pr-3">
+                        <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">New</button>
+                    </div>
+                <div>
+                    hi hello ff
+                </div>
+            </div>
+        </div>
+    </div>
+}
